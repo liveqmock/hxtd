955f9ba313583e4f7f47d6a73bf138796c08abbc
xiayx <xiayouxue@gmail.com>
2014-07-08 08:38:00 +0800

    同步到git

--- docs/mine/sql-server/temp.sql
+++ docs/mine/sql-server/temp.sql
@@ -22,12 +22,18 @@
 
 # 个人设置
 
-use hxtd;
-INSERT INTO `sm_function` VALUES ('110106', '1101', '10406', '授权', '/system/user/authorization.do', '', '1', '2014-05-26 00:00:00', '1', '2014-05-26 19:29:06', '1060103', '0', NULL, '1');
-INSERT INTO `sm_function` VALUES ('110107', '1101', '110107', '重置密码', '/system/user/resetPassword.do', '', '1', '2014-05-31 00:00:00', '1', '2014-05-31 06:39:39', '1060103', '0', NULL, '1');
-INSERT INTO `sm_function` VALUES ('110108', '1101', '110108', '启用', '/system/user/enable.do', '', '1', '2014-05-31 00:00:00', '1', '2014-05-31 06:39:39', '1060103', '0', NULL, '1');
-INSERT INTO `sm_function` VALUES ('110109', '1101', '110109', '禁用', '/system/user/disable.do', '', '1', '2014-05-31 00:00:00', '1', '2014-05-31 06:39:39', '1060103', '0', NULL, '1');
-INSERT INTO `sm_function` VALUES ('110110', '1101', '110110', '登录', '/login.doself', '', '1', '2014-05-31 00:00:00', '1', '2014-06-05 14:29:10', '1060101', '0', NULL, '1');
-INSERT INTO `sm_function` VALUES ('110111', '1101', '110111', '注销', '/logout.doself', '', '1', '2014-05-31 00:00:00', '1', '2014-06-05 14:29:13', '1060101', '0', NULL, '1');
-INSERT INTO `sm_function` VALUES ('110112', '1101', '110112', '导入', '/system/user/import.do', '', '1', '2014-05-31 00:00:00', '1', '2014-06-05 14:29:01', '1060103', '0', NULL, '1');
-INSERT INTO `sm_function` VALUES ('110113', '1101', '110113', '用户导出', '/system/user/export.do', '', '1', '2014-05-31 00:00:00', '1', '2014-06-13 18:40:58', '1060103', '0', '6', '1');
+CREATE TABLE hxtd.`sm_audit_log` (
+  `ID` bigint(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `MODULE_NAME` varchar(64) NOT NULL DEFAULT '' COMMENT '日志名称',
+  `RECORD_ID` int(11) DEFAULT '0' COMMENT '日志内容',
+  `RECORD_NAME` varchar(64) NOT NULL DEFAULT '',
+  `TYPE` int(11) NOT NULL DEFAULT '0' COMMENT '操作类型',
+  `REMARK` varchar(64) DEFAULT NULL,
+  `CREATOR_ID` int(11) NOT NULL DEFAULT '0' COMMENT '用户ID',
+  `CREATED_TIME` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '操作时间',
+  PRIMARY KEY (`ID`),
+  KEY `INDEX_USER_ID` (`CREATOR_ID`),
+  KEY `INDEX_MODULE_NAME` (`MODULE_NAME`) USING BTREE,
+  KEY `INDEX_TYPE` (`TYPE`),
+  KEY `INDEX_CREATED_TIME` (`CREATED_TIME`)
+) ENGINE=InnoDB AUTO_INCREMENT=238 DEFAULT CHARSET=utf8 COMMENT='审计日志表';

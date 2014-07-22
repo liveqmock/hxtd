108961d18b54431e2b4e0386e1af2838a1a679ea
xiayx <xiayouxue@gmail.com>
2014-07-22 08:37:32 +0800

    同步至git

---
+++ docs/mine/sql-server/wf_node.sql
@@ -0,0 +1,6 @@
+INSERT INTO hxtd.wf_node (ID, CODE, NAME, MODULE_ID, ROLE_ID) VALUES (1, '001', '理财经理', 1, 0);
+INSERT INTO hxtd.wf_node (ID, CODE, NAME, MODULE_ID, ROLE_ID) VALUES (2, '002', '投资经理', 1, 0);
+INSERT INTO hxtd.wf_node (ID, CODE, NAME, MODULE_ID, ROLE_ID) VALUES (3, '003', '投资总监', 1, 0);
+INSERT INTO hxtd.wf_node (ID, CODE, NAME, MODULE_ID, ROLE_ID) VALUES (4, '004', '副总经理', 1, 0);
+INSERT INTO hxtd.wf_node (ID, CODE, NAME, MODULE_ID, ROLE_ID) VALUES (5, '005', '总裁', 1, 0);
+INSERT INTO hxtd.wf_node (ID, CODE, NAME, MODULE_ID, ROLE_ID) VALUES (6, '006', '财务', 1, 0);

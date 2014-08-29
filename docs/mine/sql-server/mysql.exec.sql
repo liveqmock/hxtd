6cdc2d0da69abd02cba3a9e6a6cd0c1d8c4fd207
xiayx <keepquilt@163.com>
2014-08-29 19:30:04 +0800

    同步至git

--- docs/mine/sql-server/mysql.exec.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  this_.ID AS y0_
-FROM hxtd.SM_FUNCTION this_ INNER JOIN hxtd.SM_MENU menu3_ ON this_.MENU_ID = menu3_.ID
-  INNER JOIN hxtd.SM_ROLE_FUNC roles6_ ON this_.ID = roles6_.FUNC_ID
-  INNER JOIN hxtd.SM_ROLE role1_ ON roles6_.ROLE_ID = role1_.ID
-  INNER JOIN hxtd.SM_USER_ROLE owners8_ ON role1_.ID = owners8_.ROLE_ID
-  INNER JOIN hxtd.SM_USER owner2_ ON owners8_.USER_ID = owner2_.ID
-WHERE owner2_.ID = 4
